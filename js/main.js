b57ac7642554fbe86d8650c4425916946c220624
Eduardo Lacerda <eduardo.filho@bistowbis.com.br>
2021-08-27 10:15:30 -0300

    Atualização do conteúdo
    Realizada atualização dos conteúdos das páginas e incluiuda verificação do e-mail solicitado na página de contatos

--- js/main.js
+++ js/main.js
@@ -1,18 +1,37 @@
 var nav = document.querySelector('.top-nav');
 var mask = document.querySelector('.mask');
-var btn = document.querySelector('.burger-button');
+var btnForm = document.querySelector('.btn');
+var btnBurger = document.querySelector('.burger-button');
+var email = document.querySelector('#email');
 
-btn.addEventListener('click', function() {
+btnForm.addEventListener('click', function() {
+    const e = /[a-z0-9.]*@[a-z0-9]*\.[a-z\.]*/
+    const isValid = e.test(email.value);
+
+    if (!isValid) {
+        email.setCustomValidity('');
+
+        if (email.validity.valueMissing) {
+            email.setCustomValidity('Digite seu e-mail aqui');
+        } else {
+            email.setCustomValidity('Email inválido. Digite um e-mail como o exemplo: 12345@exemplo.com');
+        }
+    } else {
+        email.setCustomValidity('');
+    }
+});
+
+btnBurger.addEventListener('click', function() {
     nav.toggleAttribute('opened');
     mask.toggleAttribute('opened');
-    teste();
-})
+    clickOut();
+});
 
-function teste() {
+function clickOut() {
     window.addEventListener('click', function(e) {
         if (!nav.contains(e.target.parentNode) && e.target != btn) {
             nav.removeAttribute('opened');
             mask.removeAttribute('opened');
         }
-    })
+    });
 }
